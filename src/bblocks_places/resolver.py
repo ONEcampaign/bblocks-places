e5ef14e7c66e5218f435106ccd11a1e0eafd545a
Luca Picci <lpicci96@gmail.com>
2025-04-22 10:21:11 +0200

    build resolver class

--- src/bblocks_places/resolver.py
+++ src/bblocks_places/resolver.py
@@ -1,193 +1,267 @@
-"""module with functionality to resolve places
-
-Uses the datacommons API to resolve places.
-
-"""
+""" """
 
 from datacommons_client.client import DataCommonsClient
-import datacommons_client.models.resolve
-import datacommons_client.models.node
 from typing import Optional, Literal
 
 from bblocks_places.config import logger
 
 
+def _remove_duplicates_in_place(lst: list[str | list | None]) -> None:
+    """Remove duplicate values from a list in-place, preserving order.
+    Works with unhashable and nested items (e.g. lists, dicts).
+    """
+    seen = []
+    for item in lst:
+        if item not in seen:
+            seen.append(item)
+    lst[:] = seen  # in-place update
+
+
+def flatten_dict(d: dict[str: list[str, None]]) -> dict[str, str | None | list[str]]:
+    """Take a dictionary with values as a list and flatten it.
+
+    - If the list is empty, set the value to None
+    - If there are null values in the list, remove them
+    """
+
+    # if the dictionary is empty, return an empty dictionary
+    if not d:
+        return {}
+
+    # loop through the dictionary
+    for k, v in d.items():
+
+        # remove any duplicates from the list
+        _remove_duplicates_in_place(v)
+
+        # check if there are any Nones in the list and remove them
+        for i in v:
+            if i is None:
+                v.remove(i)
+
+        # check if the list is empty
+        if not v:
+            d[k] = None
+
+        # if there is only 1 value, return it as a string
+        elif len(v) == 1:
+            d[k] = v[0]
+
+        # if there are multiple values, return it as a list
+        elif len(v) > 1:
+            d[k] = v
+
+    return d
+
+
+#function to map values in dict 2 to the values in dict 1
+def map_dict(dict1: dict, dict2: dict) -> dict[str, list[str]]:
+    """Map the values in s second dictionary to the values in the first dictionary where the
+    values in the first dictionary are the keys in the second dictionary.
+
+    e.g.
+    dict1 ={
+        'a': ['b', 'c'],
+        'b': ['d', 'e']
+    }
+
+    dict2 = {
+        'b': 'x',
+        'c': 'y',
+        'd': 'z'
+    }
+
+    map_dict(dict1, dict2) = {
+        'a': ['x', 'y'],
+        'b': ['z']
+    }
+    """
+
+    return {
+        k: [dict2[id_] for id_ in v if id_ in dict2]
+        for k, v in dict1.items()
+    }
+
+
 class PlaceResolver:
-    """class that wraps the resolver endpoint to add functionality to convert countries"""
+    """A class to resolve places to their Data Commons IDs and other properties."""
 
+    def __init__(self,
+                 dc_instance: str | None = "datacommons.one.org",
+                 api_key: str | None = None,
+                 url: str | None = None):
 
-    def __init__(self, dc_instance: str | None = "datacommons.one.org", api_key: str | None = None,  url: str | None = None):
-        # instantiate the client specific to the resolve endpoint
-        self._client = DataCommonsClient(
-            dc_instance=dc_instance,
-            api_key=api_key,
-            url=url
-        )
+        self._client = DataCommonsClient(dc_instance=dc_instance, api_key=api_key, url=url)
 
-    def _fetch_candidates(self, places: str | list, place_type: Optional[str] = None) -> dict[str, list[str]]:
-        """Get the candidates for places using the resolve endpoint
+    def _fetch_dcid_candidates(self, places, place_type = None) -> dict[str, list[str]]:
+        """Return a dictionary mapping places to dcid candidates
 
         Args:
-            places: list of places to resolve
-            place_type: type of place to resolve
+            places: The place or list of places to convert
+            place_type: The type of place to convert to. This must be a Data Commons entity type for example Country
 
         Returns:
-            dictionary with the place as key and the candidates as values in a list
+            A dictionary mapping the places to the dcid candidates
         """
 
-        result = self._client.resolve.fetch_dcids_by_name(places, entity_type=place_type).entities
+        result = self._client.resolve.fetch_dcids_by_name(names=places, entity_type=place_type).to_dict()
+        return {i['node']: [j['dcid'] if len(j)>0 else None
+                    for j in i['candidates']]
+                for i in result['entities']
+                }
 
-        return {entity.node: [candidate.dcid for candidate in entity.candidates] for entity in result}
-
-
-
-    def _convert_candidates(self, places: dict, to_property: str) -> dict[str, list[str]]:
-        """For the returned candidates get the value of a property
+    def _fetch_property_map(self, dcids: list[str], dc_property: str) -> dict[str, list[str]]:
+        """Fetch the property map for a list of dcids and a property
 
         Args:
-            places: dictionary with the place as key and the candidates as values in a list
-            to_property: property to get the value of
+            dcids: The list of dcids to fetch the property map for
+            dc_property: The property to fetch the map for
 
         Returns:
-            dictionary with the place as key and converted candidates as values in a list
+            A dictionary mapping the dcids to the property values
         """
 
-        # Get all the candidates in a flat list
-        candidates = [item for sublist in places.values() for item in sublist]
+        result = self._client.node.fetch_property_values(node_dcids=dcids, properties=dc_property).to_dict()
 
-        # Get the properties for the candidates
-        result = self._client.node.fetch_property_values(node_dcids = candidates, properties = to_property)
-        mapper = {} # map the candidate to the property value
-        for i, j in result.get_properties().items():
-            if hasattr(j, "value"):
-                mapper.update({i: j.value})
+        # Get the properties for the dcids
+        d = {}
+        for dcid, value in result['data'].items():
+            if "arcs" in value:
+                d[dcid] = [node['value'] if "value" in node else node['name'] for node in value['arcs'][dc_property]['nodes']]
             else:
-                mapper.update({i: None})
+                logger.debug(f"Property {dc_property} not found for {dcid}. Resolving to None")
 
-        return {
-            key: [mapper.get(val) for val in vals if mapper.get(val) is not None]
-            for key, vals in places.items()
-        }
+        return flatten_dict(d)
 
-    def _get_conversion_map(self,
-                            places_with_candidates: dict[str, list[str]],
-                            not_found: Optional[str | Literal["raise"] | None] = "raise",
-                            multiple_candidates: Optional[Literal["raise", "first"]] = "raise") -> dict[str, [str | None]]:
-        """Get the conversion map for the places with candidates
 
-        This method parses the places and their candidates to create a flat dictionary to map the places to the candidates.
-        It handles the following cases:
-        - if there is only one candidate, map the place to the candidate
-        - if there are no candidates, map the place to the not_found value. By default this is "raise" which raises an error. Other options are None or a string to map the place to.
-        - if there are multiple candidates, raise an error or use the first candidate. By default this is "raise" which raises an error. Other options are "first" to use the first candidate.
+    def get_candidates(self,
+                       places: str | list[str],
+                       to: Optional[str] = "dcid",
+                       place_type: Optional[str] = None
+                       ) -> dict[str, str | None | list[str]]:
+        """Get the candidates a place or places in a specified format
 
         Args:
-            places_with_candidates: dictionary with the place as key and the candidates as values in a list
-            not_found: value to map the place to if there are no candidates. By default this is "raise" which raises an error. Other options are None or a string to map the place to.
-            multiple_candidates: value to handle multiple candidates. By default this is "raise" which raises an error. Other options are "first" to use the first candidate.
+            places: The place or list of places to convert
+            to: The format to convert to. Defaults to "dcid" which is the DataCommons ID.
+            place_type: The type of place to convert to. This must be a Data Commons entity type for example Country
 
         Returns:
-            dictionary with the place as key and the candidates as values in a list
-
-        Raises:
-            ValueError: if there are multiple candidates and multiple_candidates is "raise" TODO: change to custom error
-            ValueError: if there are no candidates and not_found is "raise" TODO: change to custom error
-
+            A dictionary mapping the places to the candidates in the specified format
         """
 
-        mapper = {} # map the place to the candidate
-
-        for place, candidates in places_with_candidates.items():
-            if len(candidates) == 1:
-                mapper[place] = candidates[0]
-
-            # when there are no candidates map to the user import or raise error
-            if len(candidates) == 0:
-                if not_found == "raise":
-                    raise ValueError(f"No candidates for {place}") # TODO: change to custom error
-                # if not_found is None, map the place to None
-                elif not_found is None:
-                    logger.warn(f"No candidates for {place}. Mapping to None")
-                    mapper[place] = None
-
-                else:
-                    logger.warn(f"No candidates for {place}. Mapping to {not_found}")
-                    mapper[place] = not_found
-
-            if len(candidates) > 1:
-                if multiple_candidates == "raise":
-                    raise ValueError(f"Multiple candidates for {place}: {candidates}") # TODO: change to custom error
-
-                # if multiple_candidates is "first", map the place to the first candidate
-                if multiple_candidates == "first":
-                    logger.warn(f"Multiple candidates for {place}: {candidates}. Using first candidate: {candidates[0]}")
-                    mapper[place] = candidates[0]
-
-        return mapper
-
-    def get_candidates(self, places: str | list[str], to: str = "dcid", place_type: str = None) -> dict[str, list[str]]:
-        """Get candidates for places
-
-        This method fetches candidates that match the given places in a specific format.
-
-        Args:
-            places: The place(s) to resolve
-            to: The format to convert the places to. Default is "dcid". Any other Data Commons property can be used. TODO: add a list of custom formats kept locally eg income level
-            place_type: The type of place to resolve e.g. Country. Default is None which searches all types of places.
-
-        Returns:
-            A dictionary with the place as key and the candidates in a specified format as values in a list.
-
-        """
-
-        # check if the places are a string or a list
-        if isinstance(places, str):
-            places = [places]
-
-        # remove duplicates
-        places_to_convert = list(set(places))
-
-        # fetch the candidates
-        result = self._fetch_candidates(places = places_to_convert, place_type = place_type)
+        # fetch the dcid candidates for the places
+        places_dcids_dict = self._fetch_dcid_candidates(places, place_type)
 
         if to == "dcid":
-            return result # avoid requesting DCIDs again
+            return flatten_dict(places_dcids_dict)
 
-        # TODO add functionality for custom formats such as income level
+        # get a list of all the dcids
+        dcids_list = [item for v in places_dcids_dict.values() if v for item in v]
 
-        # convert the candidates to the new format from Data Commons
-        return self._convert_candidates(result, to)
+        # get the properties for the dcids
+        dcids_dict = self._fetch_property_map(dcids_list, to)
 
-    def convert(self, places: str | list[str], to: str = "dcid", place_type: Optional[str]= None,
-                not_found: Optional[str | Literal["raise"] | None] = "raise",
-                multiple_candidates: Optional[Literal["raise", "first"]] = "raise") -> str | list[str | None]:
-        """Convert places to a new format
+        # map the values in the dcids_dict to the values in the places_dcids_dict
+        mapped_dict = map_dict(places_dcids_dict, dcids_dict)
 
-        This method converts places to a new format using the Data Commons API.
+        # flatten the dictionary
+        mapped_dict = flatten_dict(mapped_dict)
+
+        return mapped_dict
+
+    @staticmethod
+    def _parse_ambiguous_candidates(candidates: dict,
+                                    to:str,
+                                    not_found: Optional[Literal["raise", "ignore"] | str] = "raise",
+                                    multiple_candidates: Optional[Literal["raise", "first", "ignore"]] = "raise",) -> dict:
+        """Parse the candidates to check if there are any ambiguous candidates or not found candidates and handle them how the user wants
 
         Args:
-            places: The place(s) to convert
-            to: The format to convert the places to. Default is "dcid". Any other Data Commons property can be used. TODO: add a list of custom formats kept locally eg income level
-            place_type: The type of place to resolve e.g. Country. Default is None which searches all types of places.
-            not_found: value to map the place to if there are no candidates. By default this is "raise" which raises an error. Other options are None or a string to map the place to.
-            multiple_candidates: value to handle multiple candidates. By default this is "raise" which raises an error. Other options are "first" to use the first candidate.
+            candidates: The candidates to parse
+            to: The format the candidates are converted to
+            not_found: How to handle not found candidates. Can be "raise", "ignore" or a string to set the value to.
+            multiple_candidates: How to handle multiple candidates. Can be "raise", "first" or "ignore".
 
         Returns:
-            A dictionary with the place as key and the converted candidates as values in a list.
-
-        Raises:
-            ValueError: if there are multiple candidates and multiple_candidates is "raise" TODO: change to custom error
-            ValueError: if there are no candidates and not_found is "raise" TODO: change to custom error
+            The parsed candidates
         """
 
-        # Get the candidates for the places
-        result = self.get_candidates(places = places, to = to, place_type = place_type)
+        for k,v in candidates.items():
 
-        # get a mapping of the candidates to the new format
-        mapper = self._get_conversion_map(result, not_found = not_found, multiple_candidates = multiple_candidates)
+            # if the candidate is not found
+            if v is None:
+                if not_found == "raise":
+                    raise ValueError(f'Could not find a "{to}" match for: {k} ')
+                elif not_found == "ignore":
+                    logger.warn(f'Could not find a "{to}" match for: {k}. Ignoring. The value will be set to None')
+                else:
+                    logger.warn(f'Could not find a "{to}" match for: {k}. The value will be set to {not_found}')
+                    candidates[k] = not_found
 
-        # convert the places to the new format in their original type e.g. list or str
-        if isinstance(places, str):
-            return mapper.get(places)
+            # if there are multiple candidates
+            elif isinstance(v, list):
+                if multiple_candidates == "raise":
+                    raise ValueError(f'Found multiple "{to}" matches for: {k}. Found: {v}')
+                elif multiple_candidates == "first":
+                    logger.warn(f'Found multiple matches for "{to}" for: {k}. Using the first match: {v[0]}')
+                    candidates[k] = v[0]
+                elif multiple_candidates == "ignore":
+                    logger.warn(f'Found multiple "{to}" matches for: {k}. All candidates will be used: {v}')
+
+        return candidates
+
+    def convert(self,
+                places,
+                to: str = "dcid",
+                place_type: Optional[str] = None,
+                not_found: Optional[Literal["raise", "ignore"] | str] = "raise",
+                multiple_candidates: Optional[Literal["raise", "first", "ignore"]] = "raise",
+                custom_mapping: Optional[dict] = None
+                ) -> str | list[str] | None:
+        """Convert a place or list of places to a specified format
+
+        Args:
+            places: The place or list of places to convert
+            to: The format to convert to. Can be a string or a list of strings. Defaults to "dcid" which is the DataCommons ID.
+            place_type: The type of place to convert to. This must be a Data Commons entity type for example Country
+            not_found: How to handle not found candidates. Can be "raise", "ignore" or a string to set the value to. Defaults to "raise". "Raise" will raise an error if a place is not found, "ignore" will ignore the place and set the value to None, and a string will set the value to that string.
+
+        """
+
+        # if there are any custom mappings remove them from the places that need to be converted
+        if custom_mapping:
+            if isinstance(places, str) and places in custom_mapping.keys():
+                return custom_mapping[places]
+            elif isinstance(places, str):
+                # remove the custom mappings from the places
+                places_to_convert = [places]
+            elif isinstance(places, list):
+                # remove the custom mappings from the places
+                places_to_convert = [place for place in places if place not in custom_mapping.keys()]
+            else:
+                raise TypeError(f"places must be a string or a list of strings, not {type(places)}")
         else:
-            return [mapper.get(place) for place in places]
+            places_to_convert = places
+
+        candidates = self.get_candidates(places=places_to_convert, to=to, place_type=place_type)
+
+        # check if the candidates are ambiguous or not found
+        candidates = self._parse_ambiguous_candidates(candidates=candidates, to=to, not_found=not_found, multiple_candidates=multiple_candidates)
+
+        # if there are any custom mappings, add them to the candidates
+        if custom_mapping:
+            candidates = candidates | custom_mapping
+
+
+        if isinstance(places, list):
+            converted_places = [candidates.get(p, p) for p in places]
+
+        elif isinstance(places, str):
+            converted_places = candidates.get(places, places)
+
+        else:
+            raise TypeError(f"places must be a string or a list of strings, not {type(places)}")
+
+        return converted_places
+
